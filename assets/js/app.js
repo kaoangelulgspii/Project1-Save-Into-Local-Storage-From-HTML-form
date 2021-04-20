8197b0a820247c24aa141b27765d3e83865f3cae
Angelu S. Legaspi <legaspi.angelu.k178@gmail.com>
2021-04-20 23:18:17 +0800

    Adding Elements into Local Storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,6 +32,8 @@ function newTweet(e){
 
     //Add to the list
     tweetList.appendChild(li);
+    // Add to Local Storage
+    addTweetLocalStorage(tweet);
 }
 
 // Removes the tweet from the DOM
@@ -39,4 +41,27 @@ function removeTweet(e){
     if(e.target.classList.contains('remove-tweet')){
      e.target.parentElement.remove();
     }
+}
+// Adds the tweets into the local storage
+function addTweetLocalStorage(tweet){
+    let tweets = getTweetsFromStorage();
+   
+    // Add the tweet into the array
+    tweets.push(tweet);
+
+    //convert tweet array into string
+    localStorage.setItem('tweets',JSON.stringify(tweets));
+}
+
+function getTweetsFromStorage(){
+    let tweets;
+    const tweetsLS = localStorage.getItem('tweets');
+    //Get the values, if null is returned then we create an empty array
+    if(tweetsLS === null){
+        tweets = [];
+    }
+    else {
+        tweets =JSON.parse(tweetsLS);
+    }
+    return tweets;
 }
